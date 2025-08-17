1766de964e8463b9101d4e47e0662b94984d9352
rinkusharma8786 <rinkusharma8786@gmail.com>
2025-08-17 16:45:32 +0530

    add models

--- Backend/core/models.py
+++ Backend/core/models.py
@@ -32,9 +32,9 @@ class User(AbstractBaseUser):
     email = models.EmailField(unique=True, max_length=254)
     name = models.CharField(max_length=254)
     username=models.CharField(max_length=254)
-    address = models.CharField(max_length=50,blank=True,null=True)
+    address = models.CharField(max_length=50)
 
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)  # default should be False
     is_customer = models.BooleanField(default=False)
@@ -44,12 +44,12 @@ class User(AbstractBaseUser):
     updated_at = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name','username','address']
+    REQUIRED_FIELDS = ['name','address']
 
     objects = UserManager()
 
     def __str__(self):
-        return str(self.id)
+        return str(self.email)
 
     def has_perm(self, perm, obj=None):
         return self.is_superuser
@@ -57,3 +57,22 @@ class User(AbstractBaseUser):
     def has_module_perms(self, app_label):
         return self.is_superuser  # or add self.is_staff if needed
 
+class Item(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    min_price = models.DecimalField(max_digits=10, decimal_places=2)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='items_sold')
+    is_sold = models.BooleanField(default=False)
+    auction_end_time = models.DateTimeField(null=True, blank=True)
+    winner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    def __str__(self):
+        return str(self.title)
+class ItemImage(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='item_images/')
+
+class Bid(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    bid_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    bid_time = models.DateTimeField(auto_now_add=True)
